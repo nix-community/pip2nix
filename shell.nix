afa50990dcce2d6ff3342742d07d15730e5cdbf3
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-07-03 20:24:20 +0200

    src = null in nix-shell

--- shell.nix
+++ shell.nix
@@ -2,5 +2,6 @@ let
   pkgs = import <nixpkgs> {};
   pip2nix = import ./.;
 in pkgs.lib.overrideDerivation pip2nix (a: {
+  src = null;
   nativeBuildInputs = a.nativeBuildInputs ++ [pkgs.pythonPackages.ipdb];
 })
