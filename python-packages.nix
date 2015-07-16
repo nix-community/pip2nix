d8d037d6f80ea2d42bc91435080f80e7fd15321c
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-07-16 07:40:05 +0200

    Bump requirements

--- python-packages.nix
+++ python-packages.nix
@@ -1,10 +1,10 @@
 {
   pip = self.buildPythonPackage {
     doCheck = false;
-    name = "pip-7.0.3";
+    name = "pip-7.1.0";
     src = fetchurl {
-      url = "https://pypi.python.org/packages/source/p/pip/pip-7.0.3.tar.gz";
-      md5 = "54cbf5ae000fb3af3367345f5d299d1c";
+      url = "https://pypi.python.org/packages/source/p/pip/pip-7.1.0.tar.gz";
+      md5 = "d935ee9146074b1d3f26c5f0acfd120e";
     };
     propagatedBuildInputs = with self; [];
     buildInputs = with self; [];
@@ -21,20 +21,20 @@
 
   pytest = self.buildPythonPackage {
     doCheck = false;
-    name = "pytest-2.7.1";
+    name = "pytest-2.7.2";
     src = fetchurl {
-      url = "https://pypi.python.org/packages/source/p/pytest/pytest-2.7.1.tar.gz";
-      md5 = "e972d691ff6779ffb4b594449bac3e43";
+      url = "https://pypi.python.org/packages/source/p/pytest/pytest-2.7.2.tar.gz";
+      md5 = "dcd8e891474d605b81fc7fcc8711e95b";
     };
     propagatedBuildInputs = with self; [py];
     buildInputs = with self; [];
   };
   py = self.buildPythonPackage {
     doCheck = false;
-    name = "py-1.4.28";
+    name = "py-1.4.30";
     src = fetchurl {
-      url = "https://pypi.python.org/packages/source/p/py/py-1.4.28.tar.gz";
-      md5 = "30b807e1fe1b886578c47337d424a083";
+      url = "https://pypi.python.org/packages/source/p/py/py-1.4.30.tar.gz";
+      md5 = "a904aabfe4765cb754f2db84ec7bb03a";
     };
     propagatedBuildInputs = with self; [];
     buildInputs = with self; [];
