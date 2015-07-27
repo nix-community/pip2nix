ad737403e5010453b3ee6386ddc29a348f5d3add
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-07-28 00:17:00 +0200

    Stop looking for config file only if [pip2nix*] found

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,6 @@
+import pytest
+import os
+
 from pip2nix.config import Config
 
 
@@ -16,6 +19,13 @@ class CallableMiniMock(object):
 
     def __call__(self, *args, **kwargs):
         self.__calls.append((args, kwargs))
+
+
+@pytest.yield_fixture
+def cwd():
+    old_cwd = os.getcwd()
+    yield os.chdir
+    os.chdir(old_cwd)
 
 
 def test_merging_configs():
@@ -52,3 +62,16 @@ def test_get_package_config():
             'additional_requirements': ['nix:pkgs.postgresql']}})
     pkg_conf = c.get_package_config('psycopg2')
     assert pkg_conf == {'additional_requirements': ['nix:pkgs.postgresql']}
+
+
+def test_finding_config_file(tmpdir, cwd):
+    subdir = tmpdir.mkdir('sub')
+    subdir.join('setup.cfg').write('[default]\na = sub/setup.cfg\n')
+    subdir.join('pip2nix.ini').write('[default]\na = sub/pip2nix.ini\n')
+    tmpdir.join('pip2nix.ini').write('[pip2nix]\na = ./pip2nix.ini\n')
+
+    cwd(str(subdir))
+    c = Config()
+    c.find_and_load()
+
+    assert c['pip2nix']['a'] == './pip2nix.ini'
