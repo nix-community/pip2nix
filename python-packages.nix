61d80bbc8f705d72c9a7b84a7c88fa5643cdbf89
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-09-02 23:12:37 +0200

    Bump dependencies

--- python-packages.nix
+++ python-packages.nix
@@ -1,83 +1,83 @@
 {
+  click = super.buildPythonPackage {
+    name = "click-5.1";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [];
+    src = fetchurl {
+      url = "https://pypi.python.org/packages/source/c/click/click-5.1.tar.gz";
+      md5 = "9c5323008cccfe232a8b161fc8196d41";
+    };
+  };
   configobj = super.buildPythonPackage {
     name = "configobj-5.0.6";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [six];
     src = fetchurl {
       url = "https://pypi.python.org/packages/source/c/configobj/configobj-5.0.6.tar.gz";
       md5 = "e472a3a1c2a67bb0ec9b5d54c13a47d6";
     };
-    propagatedBuildInputs = with self; [six];
-    buildInputs = with self; [];
-    doCheck = false;
-  };
-  six = super.buildPythonPackage {
-    name = "six-1.9.0";
-    src = fetchurl {
-      url = "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz";
-      md5 = "476881ef4012262dfc8adc645ee786c4";
-    };
-    propagatedBuildInputs = with self; [];
-    buildInputs = with self; [];
-    doCheck = false;
-  };
-  pip2nix = super.buildPythonPackage {
-    name = "pip2nix-0.2.0.dev1";
-    src = ./.;
-    propagatedBuildInputs = with self; [pip configobj click contexter];
-    makeWrapperArgs = "--prefix PATH : ${pkgs.nix-prefetch-scripts}";
-    buildInputs = with self; [pytest];
-    doCheck = true;
-  };
-  pip = super.buildPythonPackage {
-    name = "pip-7.1.0";
-    src = fetchurl {
-      url = "https://pypi.python.org/packages/source/p/pip/pip-7.1.0.tar.gz";
-      md5 = "d935ee9146074b1d3f26c5f0acfd120e";
-    };
-    propagatedBuildInputs = with self; [];
-    buildInputs = with self; [];
-    doCheck = false;
   };
   contexter = super.buildPythonPackage {
     name = "contexter-0.1.3";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [];
     src = fetchurl {
       url = "https://pypi.python.org/packages/source/c/contexter/contexter-0.1.3.tar.gz";
       md5 = "437efd28f5489cccfe929c08c6b269aa";
     };
-    propagatedBuildInputs = with self; [];
-    buildInputs = with self; [];
-    doCheck = false;
   };
-  click = super.buildPythonPackage {
-    name = "click-4.1";
-    src = fetchurl {
-      url = "https://pypi.python.org/packages/source/c/click/click-4.1.tar.gz";
-      md5 = "6a3fa88c738f2f775ec6de126feb99a4";
-    };
-    propagatedBuildInputs = with self; [];
+  pip = super.buildPythonPackage {
+    name = "pip-7.1.2";
     buildInputs = with self; [];
     doCheck = false;
+    propagatedBuildInputs = with self; [];
+    src = fetchurl {
+      url = "https://pypi.python.org/packages/source/p/pip/pip-7.1.2.tar.gz";
+      md5 = "3823d2343d9f3aaab21cf9c917710196";
+    };
+  };
+  pip2nix = super.buildPythonPackage {
+    name = "pip2nix-0.2.0.dev1";
+    buildInputs = with self; [pytest];
+    doCheck = true;
+    makeWrapperArgs = "--prefix PATH : ${pkgs.nix-prefetch-scripts}";
+    propagatedBuildInputs = with self; [pip configobj click contexter];
+    src = ./.;
+  };
+  six = super.buildPythonPackage {
+    name = "six-1.9.0";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [];
+    src = fetchurl {
+      url = "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz";
+      md5 = "476881ef4012262dfc8adc645ee786c4";
+    };
   };
 
 ### Test requirements
 
   py = super.buildPythonPackage {
     name = "py-1.4.30";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [];
     src = fetchurl {
       url = "https://pypi.python.org/packages/source/p/py/py-1.4.30.tar.gz";
       md5 = "a904aabfe4765cb754f2db84ec7bb03a";
     };
-    propagatedBuildInputs = with self; [];
-    buildInputs = with self; [];
-    doCheck = false;
   };
   pytest = super.buildPythonPackage {
     name = "pytest-2.7.2";
+    buildInputs = with self; [];
+    doCheck = false;
+    propagatedBuildInputs = with self; [py];
     src = fetchurl {
       url = "https://pypi.python.org/packages/source/p/pytest/pytest-2.7.2.tar.gz";
       md5 = "dcd8e891474d605b81fc7fcc8711e95b";
     };
-    propagatedBuildInputs = with self; [py];
-    buildInputs = with self; [];
-    doCheck = false;
   };
 }
