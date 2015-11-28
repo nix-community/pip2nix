a2263893465434966e9e0edd8f0e6566f4814f38
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-11-28 19:25:47 +0100

    Removed shell.nix to test the new default.nix

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? (import <nixpkgs> {}), pythonPackages ? "python27Packages" }@args:
-let
-  pip2nix = import ./. args;
-in pkgs.lib.overrideDerivation pip2nix (a: {
-  src = null;
-  #nativeBuildInputs = a.nativeBuildInputs ++ [pkgs.pythonPackages.ipdb];
-})
