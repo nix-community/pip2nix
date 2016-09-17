6f2293b7da37588f4f56140d1799ce8ac9db7f47
Johannes Bornhold <johannes@bornhold.name>
2016-09-17 21:25:49 +0200

    Update release-shell.nix to python35

--- release-shell.nix
+++ release-shell.nix
@@ -1,14 +1,16 @@
 { pkgs ? (import <nixpkgs> {}) }:
 
 let
-  pythonPackages = pkgs.python34Packages.override (a: {
+  pythonPackages = pkgs.python35Packages.override (a: {
     self = pythonPackages;
   }) // (scopedImport {
     self = pythonPackages;
-    super = pkgs.python34Packages;
+    super = pkgs.python35Packages;
     inherit pkgs;
     inherit (pkgs) fetchurl;
-  } ./release-python-packages.nix);
+  } ./release-python-packages.nix) // {
+    inherit (pkgs.python35Packages) setuptools;
+  };
 in pkgs.stdenv.mkDerivation {
   name = "release";
   buildInputs = with pythonPackages; [ twine bumpversion ];
