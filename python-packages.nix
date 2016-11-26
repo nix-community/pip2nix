8fe9051ea9b69e27abc73c8f8240d6c3a31ce405
Johannes Bornhold <johannes@bornhold.name>
2016-11-26 20:14:43 +0100

    Bump version: 0.6.0.dev1 → 0.6.1

--- python-packages.nix
+++ python-packages.nix
@@ -63,7 +63,7 @@
     };
   };
   pip2nix = super.buildPythonPackage {
-    name = "pip2nix-0.6.0.dev1";
+    name = "pip2nix-0.6.1";
     buildInputs = with self; [pytest];
     doCheck = true;
     makeWrapperArgs = "--prefix PATH : ${pkgs.nix-prefetch-scripts}";
