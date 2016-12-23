30f8690373c7ef49dc8b4a49e198819864372729
Johannes Bornhold <johannes@bornhold.name>
2016-12-23 02:30:03 +0100

    Bump version: 0.6.0 → 0.7.0.dev1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), 'rb') as f:
     long_description = f.read().decode('utf-8')
 
-VERSION = '0.6.0'
+VERSION = '0.7.0.dev1'
 
 setup(
     name="pip2nix",
