49f9ec477aaaa34d4b928c7e767ee170a8a9356d
Johannes Bornhold <johannes@bornhold.name>
2018-01-02 20:32:38 +0100

    Fix up release-shell.nix

--- release-shell.nix
+++ release-shell.nix
@@ -1,17 +1,34 @@
-{ pkgs ? (import <nixpkgs> {}) }:
+{ pkgs ? (import <nixpkgs> {})
+, pythonPackages ? "python36Packages"
+}:
 
+with pkgs.lib;
 let
-  pythonPackages = pkgs.python35Packages.override (a: {
-    self = pythonPackages;
-  }) // (scopedImport {
-    self = pythonPackages;
-    super = pkgs.python35Packages;
-    inherit pkgs;
-    inherit (pkgs) fetchurl;
-  } ./release-python-packages.nix) // {
-    inherit (pkgs.python35Packages) setuptools;
+  basePythonPackages = with builtins; if isAttrs pythonPackages
+    then pythonPackages
+    else getAttr pythonPackages pkgs;
+
+  # Works with the new python-packages, still can fallback to the old
+  # variant.
+  basePythonPackagesUnfix = basePythonPackages.__unfix__ or (
+    self: basePythonPackages.override (a: { inherit self; }));
+
+  pythonPackagesLocalOverrides = self: super: {
+    setuptools = basePythonPackages.setuptools;
   };
+
+  pythonPackagesGenerated = import ./release-python-packages.nix {
+    inherit pkgs;
+    inherit (pkgs) fetchurl fetchgit fetchhg;
+  };
+
+  myPythonPackages =
+    (fix
+    (extends pythonPackagesLocalOverrides
+    (extends pythonPackagesGenerated
+             basePythonPackagesUnfix)));
+
 in pkgs.stdenv.mkDerivation {
   name = "release";
-  buildInputs = with pythonPackages; [ twine bumpversion ];
+  buildInputs = with myPythonPackages; [ twine bumpversion ];
 }
