6384d2099b833f811bee4d3034147b94656d3f73
Asko Soukka <asko.soukka@iki.fi>
2019-08-18 18:10:08 +0300

    Update Travis-CI -config

--- python-packages.nix
+++ python-packages.nix
@@ -55,13 +55,13 @@ self: super: {
     };
   };
   pip = super.buildPythonPackage {
-    name = "pip-9.0.1";
+    name = "pip-19.2.2";
     buildInputs = with self; [];
     doCheck = false;
     propagatedBuildInputs = with self; [];
     src = fetchurl {
-      url = "https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz";
-      sha256 = "03clr9c1dih5n9c00c592zzvf6r1ffimywkaq9agcqdllzhl7wh9";
+      url = "https://files.pythonhosted.org/packages/aa/1a/62fb0b95b1572c76dbc3cc31124a8b6866cbe9139eb7659ac7349457cf7c/pip-19.2.2.tar.gz";
+      sha256 = "e05103825871e210d50a44c7e448587b0ed99dd775d3ef586304c58f40224a53";
     };
   };
   pip2nix = super.buildPythonPackage {
